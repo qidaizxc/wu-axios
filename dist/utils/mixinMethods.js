b3768aedfdd6add0eba7d09629049a43b9b03a21
wutong <575650532@qq.com>
2018-06-15 15:46:10 +0800

    初始化

--- dist/utils/mixinMethods.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function mixinRequest(methods) {
-    return function (target) {
-        console.log(methods);
-        console.log(target.prototype, '@#@#@');
-    };
-}
-exports.mixinRequest = mixinRequest;
