81e4dc29fae053e7811fa226397cb6cc2130384c
qidaizxc <575650532@qq.com>
2018-06-17 21:11:19 +0800

    完成nodejs端的处理；

--- src/defaults.ts
+++ src/defaults.ts
@@ -1,11 +1,12 @@
 import xhr from "./adapters/xhr";
-
+import http from './adapters/http';
 // 根据环境获取当前请求方法
 function getDefaultAdapter():Adapter {
   let adapter: Adapter = (config: any) => {return config};
   // node.js 端
   if(typeof process !== 'undefined' && Object.prototype.toString.call(process) === '[object process]'){
-  // fetch 请求
+    adapter = http;
+    // fetch 请求
   }else if(typeof window !== 'undefined' && typeof window.fetch !== 'undefined'){
 
   // ajax请求
